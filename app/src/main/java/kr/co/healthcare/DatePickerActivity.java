8f98f28ea614815f6633cf7f840d0795ff0d0d43
Ujaa <yujinyll@naver.com>
2020-11-20 13:57:15 +0900

    튜토리얼 step1 - 생년월일 날짜 선택 기능 추가

---
+++ app/src/main/java/kr/co/healthcare/DatePickerActivity.java
@@ -0,0 +1,52 @@
+package kr.co.healthcare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class DatePickerActivity extends Activity {
+    private int mYear =0, mMonth=0, mDay=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*
+        WindowManager.LayoutParams  layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount  = 0.7f;
+        getWindow().setAttributes(layoutParams);*/
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_date_picker);
+        Calendar calendar = new GregorianCalendar();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+        DatePicker datePicker = findViewById(R.id.vDatePicker);
+        datePicker.init(mYear, mMonth, mDay,mOnDateChangedListener);
+    }
+
+    public void mOnClick(View v){
+        Intent intent = new Intent();
+        intent.putExtra("mYear",mYear);
+        intent.putExtra("mMonth",mMonth);
+        intent.putExtra("mDay", mDay);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener(){
+        @Override
+        public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
+            mYear = yy;
+            mMonth = mm;
+            mDay = dd;
+        }
+    };
+}
--- app/src/main/java/kr/co/healthcare/Tutorial1StepActivity.java
+++ app/src/main/java/kr/co/healthcare/Tutorial1StepActivity.java
@@ -1,6 +1,5 @@
 package kr.co.healthcare;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +10,26 @@ import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Tutorial1StepActivity extends AppCompatActivity {
+    private int ACT_SET_BIRTH = 1;
+    Button dateBtn;
+    private boolean isBirthChecked = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial_1_step);
 
-        //첫 번째 튜토리얼 완료 시
+        //생년월일 버튼 클릭 시
+        dateBtn = (Button) findViewById(R.id.dateBtn);
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Tutorial1StepActivity.this, DatePickerActivity.class);
+                startActivityForResult(intent, ACT_SET_BIRTH);
+            }
+        });
+
+        //첫 번째 튜토리얼 완료 버튼 클릭 시
         final Button firstStepFinishBtn = (Button) findViewById(R.id.firstStepFinishBtn);
         firstStepFinishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,19 +39,36 @@ public class Tutorial1StepActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //생년월일 데이터 받기
+                String result = data.getIntExtra("mYear", 0) + "-"
+                        + data.getIntExtra("mMonth", 1) + "-"
+                        + data.getIntExtra("mDay", 1);
+                dateBtn.setText(result);
+                isBirthChecked = true;
+            }
+        }
+    }
+
     //이름, 생년월일, 성별 데이터 저장 후 다음 튜토리얼 스텝으로 화면 변경
 
     void setDataAndChangeView(){
         EditText etName = (EditText)findViewById(R.id.editTextPersonName);
         RadioGroup rg = (RadioGroup) findViewById(R.id.rdGroup);
         try {
+
             //이름 예외처리: 최소 1글자 이상이여야 함.
             if(etName.getText().toString().length() != 0)
                 PreferenceManger.setString(this,"name", etName.getText().toString());
             else
                 throw new NameCountZeroException();
 
-            //TODO : 생년월일 예외처리: 날짜를 하나 이상 체크해야 함.
+            //생년월일 예외처리: 날짜를 체크해야 함.
+            if(!isBirthChecked) throw new NoneBirthSelectedException();
 
             //성별 예외처리: 남/여 중 하나는 체크해야 함.
             if(rg.getCheckedRadioButtonId() == (R.id.radioBtnMale))
@@ -52,6 +82,9 @@ public class Tutorial1StepActivity extends AppCompatActivity {
         }
         catch (NameCountZeroException e) {
             etName.setError("이름을 한 글자 이상 입력해 주세요.");
+        }
+        catch (NoneBirthSelectedException e) {
+            e.printStackTrace();
         }
         catch (NoneSexSelectedException e) {
             e.printStackTrace();
