25ae73f1d5b835de6f86c89e92367ad047cd080d
Hong-Jinseo <hong_jinseo@naver.com>
2021-07-13 02:10:29 +0900

    [UPDATE] 자가진단 - 자가진단 시작하는 버튼 연결 (recyclerview 내부에 정의) - 결과 확인 버튼 연결

--- app/src/main/java/kr/co/healthcare/self_diagnosis/MainRecycler/SelfMainAdapter.java
+++ app/src/main/java/kr/co/healthcare/self_diagnosis/MainRecycler/SelfMainAdapter.java
@@ -1,5 +1,7 @@
 package kr.co.healthcare.self_diagnosis.MainRecycler;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,14 +15,17 @@ import java.util.ArrayList;
 
 import kr.co.healthcare.R;
 import kr.co.healthcare.self_diagnosis.MainRecycler.SelfMainData;
+import kr.co.healthcare.self_diagnosis.SelfDiagnosisActivity;
 
 public class SelfMainAdapter extends RecyclerView.Adapter<SelfMainAdapter.MyViewHolderMain> {
 
     private ArrayList<SelfMainData> myDataList = new ArrayList<>();
     String[] disease_list = {"고혈압", "골관절염", "고지혈증", "요통/좌골신경통", "당뇨병", "골다공증", "치매"};
+    private Context mContext;
 
-    public SelfMainAdapter(ArrayList<SelfMainData> dataList){
-        myDataList = dataList;
+    public SelfMainAdapter(Context mContext, ArrayList<SelfMainData> dataList){
+        this.mContext = mContext;
+        this.myDataList = dataList;
     }
 
     @NonNull
@@ -32,11 +37,24 @@ public class SelfMainAdapter extends RecyclerView.Adapter<SelfMainAdapter.MyView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolderMain holder, int position) {
-        //holder.onBind(resultData.get(position), position);
-
+    public void onBindViewHolder(@NonNull final MyViewHolderMain holder, final int position) {
         holder.tv_diseaseName.setText(myDataList.get(position).getDisease_name());
         holder.tv_numOfQuestions.setText(myDataList.get(position).getNum_of_qeustions()+"문항");
+
+        //리스너
+        holder.itemView.setClickable(true);
+        holder.itemView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int pos = holder.getAdapterPosition();  //클릭한 아이템의 포지션 줌
+                if (pos != RecyclerView.NO_POSITION){   //포지션이 recyclerView의 item인지 아닌지 확인
+                    Intent intent = new Intent(mContext, SelfDiagnosisActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("str", myDataList.get(position).getDisease_name());
+                    intent.putExtra("disease_num", myDataList.get(position).getID());
+                    mContext.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -63,6 +81,24 @@ public class SelfMainAdapter extends RecyclerView.Adapter<SelfMainAdapter.MyView
 
             tv_diseaseName = itemView.findViewById(R.id.tv_diseaseName);
             tv_numOfQuestions = itemView.findViewById(R.id.tv_numOfQuestions);
+
+            //리스너
+            /*
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION){
+                        // click event
+                        Intent intent = new Intent(context, SelfDiagnosisActivity.class);
+                        intent.putExtra("str", buttonText);
+                        intent.putExtra("disease_num", INDEX);
+                        startActivity(intent);
+                    }
+                }
+            });
+
+             */
         }
     }
 }
--- app/src/main/java/kr/co/healthcare/self_diagnosis/SelfMainActivity.java
+++ app/src/main/java/kr/co/healthcare/self_diagnosis/SelfMainActivity.java
@@ -23,22 +23,12 @@ import java.util.List;
 
 public class SelfMainActivity extends AppCompatActivity {
 
-    private Button btn_chkDate, btn_chkSymptom;
-    private ArrayList<SelfMainData> dataList;
-    private List<Result> results;
+    Button btn_chkDate;
+    ArrayList<SelfMainData> dataList;
+    List<Result> results;
     public List<Questions> questionsList;
 
     String[] disease_list = {"고혈압", "골관절염", "고지혈증", "요통/좌골신경통", "당뇨병", "골다공증", "치매"};
-
-
-    /*
-    Button button_q[] = new Button[7];
-    Integer[] Rid_button_q = {
-            R.id.btn_01, R.id.btn_02, R.id.btn_03, R.id.btn_04,
-            R.id.btn_05, R.id.btn_06, R.id.btn_07
-    };
-
-     */
 
 
     @Override
@@ -46,55 +36,23 @@ public class SelfMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_main);
 
+        btn_chkDate = findViewById(R.id.btn_chkDate);
+
         this.InitializeData();
 
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recyclerview_selfmain);
         GridLayoutManager manager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(manager); // LayoutManager 등록
-        recyclerView.setAdapter(new SelfMainAdapter(dataList));  // Adapter 등록
-
-
-/*
-        //버튼 ID 연결
-        for (int i=0; i<7; i++) button_q[i] = findViewById(Rid_button_q[i]);
-        btn_chkDate = findViewById(R.id.btn_chkDate);
-        btn_chkSymptom = findViewById(R.id.btn_chkSymptom);
-
-        //자가진단 버튼 Listener 연결
-        for (int i=0; i<button_q.length; i++){
-            final String buttonText = button_q[i].getText().toString();
-            final int INDEX = i;
-
-            button_q[INDEX].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(SelfMainActivity.this, SelfDiagnosisActivity.class);
-                    intent.putExtra("str", buttonText);
-                    intent.putExtra("disease_num", INDEX);
-                    startActivity(intent);
-                }
-            });
-        }
+        recyclerView.setAdapter(new SelfMainAdapter(getApplicationContext(), dataList));  // Adapter 등록
 
         //결과확인 버튼 - 날짜별
-        btn_chkDate.setOnClickListener(new View.OnClickListener(){
+        btn_chkDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SelfMainActivity.this, SelfResultDateActivity.class);
                 startActivity(intent);
             }
         });
-
-        //결과확인 버튼 - 증상별
-        btn_chkSymptom.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SelfMainActivity.this, SelfResultSymptomActivity.class);
-                startActivity(intent);
-            }
-        });
-
- */
     }
 
     public void InitializeData(){
