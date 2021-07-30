5f7e0af1094192b6372cd134de502d09141651a2
Hong-Jinseo <hong_jinseo@naver.com>
2021-07-30 18:28:04 +0900

    [ADD] 게임3 레벨/게임명 코드 추가 - 결과 화면에 해당 게임의 이름 명시 - 결과 화면 배경 색 수정 - 레벨에 따라 별 개수 다르게 보이도록 수정

--- app/src/main/java/kr/co/healthcare/game/GameResultActivity.java
+++ app/src/main/java/kr/co/healthcare/game/GameResultActivity.java
@@ -1,13 +1,18 @@
 package kr.co.healthcare.game;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import kr.co.healthcare.R;
@@ -17,29 +22,32 @@ public class GameResultActivity extends AppCompatActivity {
     TextView tv_level;
     TextView tv_score;
     TextView tv_showBest;
+    TextView tv_game_type;
     Button btn_restart;
-    Button btn_end;
+    ImageButton btn_end;
+    ImageView img_star_1, img_star_2, img_star_3;
 
     //최고기록 저장용 shared preferences
     SharedPreferences game_score_pref;
     SharedPreferences.Editor editor;
 
-    int best_score_game1_lv1, best_score_game1_lv2, best_score_game1_lv3;
-    int best_score_game2_lv1, best_score_game2_lv2, best_score_game2_lv3;
-    int best_score_game3_lv1, best_score_game3_lv2, best_score_game3_lv3;
     int score, level, game, best_record;
     String best_record_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game1_result);
+        setContentView(R.layout.activity_game_result);
 
         tv_level = findViewById(R.id.tv_level);
         tv_score = findViewById(R.id.tv_score);
         tv_showBest = findViewById(R.id.tv_showBest);
+        tv_game_type = findViewById(R.id.tv_game_type);
         btn_restart = findViewById(R.id.btn_restart);
         btn_end = findViewById(R.id.btn_end);
+        img_star_1 = findViewById(R.id.img_star_1);
+        img_star_2 = findViewById(R.id.img_star_2);
+        img_star_3 = findViewById(R.id.img_star_3);
 
 
         //shared preference 초기화
@@ -51,8 +59,14 @@ public class GameResultActivity extends AppCompatActivity {
         score = getIntent().getIntExtra("score", -1);
         level = getIntent().getIntExtra("level", -1);
         game = getIntent().getIntExtra("game", -1);
-        show_level(level);
+        //show_level(level)
         save_score();
+
+        //게임 종류 보이기
+        show_game_type();
+
+        //레벨에 따른 별 보이기
+        show_level_by_star();
 
 
         //다시하기
@@ -86,13 +100,21 @@ public class GameResultActivity extends AppCompatActivity {
             editor.apply(); //저장
         }
 ;
-        tv_showBest.setText(best_record +"점");
-        tv_score.setText(score+"점");
+        tv_showBest.setText(best_record +"");
+        tv_score.setText(score+"");
     }
 
-    void show_level(int level){
-        if(level==1) tv_level.setText("쉬움");
-        else if(level==2) tv_level.setText("중간");
-        else tv_level.setText("어려움");
+    void show_level_by_star(){
+        if(level==1) {
+            img_star_2.setColorFilter(getResources().getColor(R.color.primaryLightColor));
+            img_star_3.setColorFilter(getResources().getColor(R.color.primaryLightColor));
+        }
+        else if(level==2) img_star_3.setColorFilter(getResources().getColor(R.color.primaryLightColor));
+    }
+
+    void show_game_type(){
+        if(game==1) tv_game_type.setText("하나빼기");
+        else if(game==2) tv_game_type.setText("간단 연산");
+        else tv_game_type.setText("카드 뒤집기");
     }
 }
