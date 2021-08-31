9dc48627141a3d07069ac8cf540d6f1ce2338961
Hong-Jinseo <hong_jinseo@naver.com>
2021-08-31 21:42:44 +0900

    [REFACTOR] 건강 정보 검색 기능 완성 - 별도의 액티비티로 분리

---
+++ app/src/main/java/kr/co/healthcare/healthInfo/HealthInfoSearchActivity.java
@@ -0,0 +1,50 @@
+package kr.co.healthcare.healthInfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kr.co.healthcare.R;
+import kr.co.healthcare.WebActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HealthInfoSearchActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private String url, keyword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_info_search);
+
+        keyword = getIntent().getStringExtra("keyword");
+        url = "https://www.google.com/search?q=" + keyword + "&tbm=nws";
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebActivity.WebViewClientClass());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
--- app/src/main/java/kr/co/healthcare/healthInfo/ui/main/Search.java
+++ app/src/main/java/kr/co/healthcare/healthInfo/ui/main/Search.java
@@ -1,124 +1,124 @@
-package kr.co.healthcare.healthInfo.ui.main;
-
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.webkit.WebBackForwardList;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipGroup;
-import com.google.android.material.textfield.TextInputEditText;
-
-import java.util.ArrayList;
-
-import kr.co.healthcare.R;
-import kr.co.healthcare.WebActivity;
-import kr.co.healthcare.healthInfo.HealthInfoActivity;
-
-import static android.content.Context.INPUT_METHOD_SERVICE;
-import static android.view.KeyEvent.KEYCODE_ENTER;
-
-public class Search extends Fragment {
-
-    /*
-    private WebView webView;
-    private String url;
-
-
-    private String[] keywords;
-
-    TextInputEditText textInput;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        keywords = getResources().getStringArray(R.array.SEARCH_KEYWORD_LABEL);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_search, container, false);
-
-        ChipGroup chipGroup = view.findViewById(R.id.CHIPGROUP_keywords);
-        textInput = view.findViewById(R.id.textInput);
-        webView = view.findViewById(R.id.webView);
-
-        //chip
-        for(String keyword : keywords){
-            final Chip chip = (Chip) this.getLayoutInflater().inflate(
-                    R.layout.item_chip_search_keyword, chipGroup, false);
-            chip.setText(keyword);
-            chipGroup.addView(chip);
-
-            //chip 선택 했을 경우
-            chip.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    url = "https://www.google.com/search?q=" + keyword + "&tbm=nws";
-                    initSearch();
-                    setWebView();
-                }
-            });
-        }
-
-        //검색어 입력 후 엔터 눌렀을 경우
-        textInput.setOnKeyListener(new View.OnKeyListener(){
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                switch (keyCode){
-                    case  KeyEvent.KEYCODE_ENTER:
-                        url = "https://www.google.com/search?q=" + textInput.getText() + "&tbm=nws";
-                        initSearch();
-                        setWebView();
-                        break;
-                }
-                return true;
-            }
-        });
-
-        return view;
-    }
-
-    //webView 설정 및 화면 레이아웃 구성 변경
-    void setWebView(){
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(url);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebActivity.WebViewClientClass());
-        webView.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction()!=KeyEvent.ACTION_DOWN)
-                    return true;
-                if(keyCode == KeyEvent.KEYCODE_BACK) {
-                    if (webView.canGoBack()) webView.goBack();
-                    else initLayout();
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    void initSearch(){
-        layout_init.setVisibility(View.GONE);
-        webView.setVisibility(View.VISIBLE);
-    }
-    void initLayout(){
-        webView.setVisibility(View.GONE);
-        layout_init.setVisibility(View.VISIBLE);
-    }
-
-     */
-}
+//package kr.co.healthcare.healthInfo.ui.main;
+//
+//import android.os.Bundle;
+//import android.view.KeyEvent;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.inputmethod.InputMethodManager;
+//import android.webkit.WebBackForwardList;
+//import android.webkit.WebChromeClient;
+//import android.webkit.WebView;
+//import android.webkit.WebViewClient;
+//import android.widget.LinearLayout;
+//
+//import androidx.fragment.app.Fragment;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.google.android.material.chip.Chip;
+//import com.google.android.material.chip.ChipGroup;
+//import com.google.android.material.textfield.TextInputEditText;
+//
+//import java.util.ArrayList;
+//
+//import kr.co.healthcare.R;
+//import kr.co.healthcare.WebActivity;
+//import kr.co.healthcare.healthInfo.HealthInfoActivity;
+//
+//import static android.content.Context.INPUT_METHOD_SERVICE;
+//import static android.view.KeyEvent.KEYCODE_ENTER;
+//
+//public class Search extends Fragment {
+//
+//
+//    private WebView webView;
+//    private String url;
+//
+//
+//    private String[] keywords;
+//
+//    TextInputEditText textInput;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        keywords = getResources().getStringArray(R.array.SEARCH_KEYWORD_LABEL);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_search, container, false);
+//
+//        ChipGroup chipGroup = view.findViewById(R.id.CHIPGROUP_keywords);
+//        textInput = view.findViewById(R.id.textInput);
+//        webView = view.findViewById(R.id.webView);
+//
+//        //chip
+//        for(String keyword : keywords){
+//            final Chip chip = (Chip) this.getLayoutInflater().inflate(
+//                    R.layout.item_chip_search_keyword, chipGroup, false);
+//            chip.setText(keyword);
+//            chipGroup.addView(chip);
+//
+//            //chip 선택 했을 경우
+//            chip.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    url = "https://www.google.com/search?q=" + keyword + "&tbm=nws";
+//                    initSearch();
+//                    setWebView();
+//                }
+//            });
+//        }
+//
+//        //검색어 입력 후 엔터 눌렀을 경우
+//        textInput.setOnKeyListener(new View.OnKeyListener(){
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                switch (keyCode){
+//                    case  KeyEvent.KEYCODE_ENTER:
+//                        url = "https://www.google.com/search?q=" + textInput.getText() + "&tbm=nws";
+//                        initSearch();
+//                        setWebView();
+//                        break;
+//                }
+//                return true;
+//            }
+//        });
+//
+//        return view;
+//    }
+//
+//    //webView 설정 및 화면 레이아웃 구성 변경
+//    void setWebView(){
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(url);
+//        webView.setWebChromeClient(new WebChromeClient());
+//        webView.setWebViewClient(new WebActivity.WebViewClientClass());
+//        webView.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if (event.getAction()!=KeyEvent.ACTION_DOWN)
+//                    return true;
+//                if(keyCode == KeyEvent.KEYCODE_BACK) {
+//                    if (webView.canGoBack()) webView.goBack();
+//                    else initLayout();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+//
+//    void initSearch(){
+//        layout_init.setVisibility(View.GONE);
+//        webView.setVisibility(View.VISIBLE);
+//    }
+//    void initLayout(){
+//        webView.setVisibility(View.GONE);
+//        layout_init.setVisibility(View.VISIBLE);
+//    }
+//
+//
+//}
