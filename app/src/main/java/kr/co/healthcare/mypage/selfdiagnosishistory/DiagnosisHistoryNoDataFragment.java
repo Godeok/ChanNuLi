94b997c9166d4971fb680ef5291e0e0d110e3c80
Yujin <yujinyll9148@gmail.com>
2021-08-31 13:12:52 +0900

    [UPDATE] 마이페이지 자가진단 기록에서 자가진단 안 한 항목은 자가진단 페이지로 연결

--- app/src/main/java/kr/co/healthcare/mypage/selfdiagnosishistory/DiagnosisHistoryNoDataFragment.java
+++ app/src/main/java/kr/co/healthcare/mypage/selfdiagnosishistory/DiagnosisHistoryNoDataFragment.java
@@ -12,7 +12,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import java.util.ArrayList;
+
 import kr.co.healthcare.R;
+import kr.co.healthcare.selfDiagnosis.MainRecycler.SelfMainData;
+import kr.co.healthcare.selfDiagnosis.QuestionDB.LoadDbClass;
 import kr.co.healthcare.selfDiagnosis.SelfDiagnosisActivity;
 
 public class DiagnosisHistoryNoDataFragment extends Fragment{
@@ -20,7 +24,7 @@ public class DiagnosisHistoryNoDataFragment extends Fragment{
     private int position;
     private Button button;
     private FragmentActivity fragmentActivity;
-
+    private ArrayList<SelfMainData> dataList = new ArrayList<>();
 
     DiagnosisHistoryNoDataFragment(int position, FragmentActivity fragmentActivity){
         this.position = position;
@@ -40,10 +44,15 @@ public class DiagnosisHistoryNoDataFragment extends Fragment{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        dataList = LoadDbClass.InitializeData(fragmentActivity);
         button = getView().findViewById(R.id.btn_do_self_diagnosis);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(fragmentActivity, SelfDiagnosisActivity.class);
+                intent.putExtra("str", dataList.get(position).getDisease_name());
+                intent.putExtra("disease_num", dataList.get(position).getID());
+                startActivity(intent);
             }
         });
     }
